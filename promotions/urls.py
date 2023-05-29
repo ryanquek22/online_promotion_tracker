3da47fb77dd470c50f7e561b380e3dc1a8ed3815
ryanquek22 <112756792+ryanquek22@users.noreply.github.com>
2023-05-29 15:57:29 +0800

    implemented static css files

--- promotions/urls.py
+++ promotions/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from tracker import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tracker.urls', namespace='events'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
